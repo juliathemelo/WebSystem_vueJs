a2557a221ecfb3059ce6705d53539e3e181c824d
juliathemelo <juliarexsteam@hotmail.com>
2021-10-01 18:31:15 -0300

    backend estrutura

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,22 +1,22 @@
 <template>
   <main>
     <div class="cards">
-        <h1 class="title">Cadastrar</h1>
-        <img src="../assets/eat well.png" class="icon" />
-        <p class="legend"> cadastro de novos produtos</p>
-        <button class="button cadastro"> cadastrar </button>
+      <h1 class="title">Cadastrar</h1>
+      <img src="../assets/eat well.png" class="icon" />
+      <p class="legend"> cadastro de novos produtos</p>
+      <button class="button cadastro"> cadastrar </button>
     </div>
     <div class="cards">
-        <h1 class="title">Consultar</h1>
-        <img src="../assets/eat well.png" class="icon" />
-        <p class="legend"> consultar produtos no estoque </p>
-        <button class="button cadastro"> consultar </button>
+      <h1 class="title">Consultar</h1>
+      <img src="../assets/eat well.png" class="icon" />
+      <p class="legend"> consultar produtos no estoque </p>
+      <button class="button consulta"> consultar </button>
     </div>
     <div class="cards">
-        <h1 class="title">Deletar</h1>
-        <img src="../assets/eat well.png" class="icon" />
-        <p class="legend"> deletar produtos no estoque </p>
-        <button class="button cadastro"> deletar </button>
+      <h1 class="title">Deletar</h1>
+      <img src="../assets/eat well.png" class="icon" />
+      <p class="legend"> deletar produtos no estoque </p>
+      <button class="button delete"> deletar </button>
     </div>
   </main>
 </template>
