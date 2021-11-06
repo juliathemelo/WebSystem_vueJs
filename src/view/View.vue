1999c1effdf378ec403dd0140d6f87877391e194
juliathemelo <juliarexsteam@hotmail.com>
2021-11-06 01:02:42 -0300

    avaliable team

--- src/view/View.vue
+++ src/view/View.vue
@@ -2,8 +2,9 @@
   <main class="View">
   <h1 class="title_product"> Products Available: </h1>
     <div class="components">
-      <div class="cards_product" v-for=" item in product" :key="item.name">
-        <li >{{ item.name }}</li>
+      <div class="cards_product" v-for=" item in product" :key="item._id" v-bind="get_team_name()">
+        <h1 >{{ item.name }}</h1>
+        <p> {{ team_name }}</p>
       </div>
     </div>
   </main>
@@ -15,13 +16,28 @@ import Product from '../services/productService';
 export default {
   name: 'View',
   data: () => ({
-    product: []
+    product: [],
+    team: [],
+    team_name: []
   }),
   mounted () {
     Product.get_all_product().then(res => {
-        this.product = res.data
+      this.product = res.data
+    }),
+    Product.get_all_teams().then(res => {
+      this.team = res.data
     })
+    this
   },
+  methods: {
+      get_team_name () {
+          for (var i = 0; i < this.team.lenght ; i++) {
+            if (this.team[i]._id == this.product[i].team) {
+              this.team_name = 'aaa'
+            }
+          }
+      }
+  }
 }
 </script>
 <style lang="scss">
